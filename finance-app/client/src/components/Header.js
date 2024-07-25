cf1abbbd57968941ae8cf0eae4317d04b758996e
Svar7769 <svarpatel7749@gmail.com>
2024-07-24 21:21:26 -0400

    working at last

--- finance-app/client/src/components/Header.js
+++ finance-app/client/src/components/Header.js
@@ -8,6 +8,7 @@ const Header = () => {
         <ul>
           <li><Link to="/">Dashboard</Link></li>
           <li><Link to="/transactions">Transactions</Link></li>
+          <li><Link to="/add-transaction">Add Transaction</Link></li>
           <li><Link to="/login">Login</Link></li>
           <li><Link to="/register">Register</Link></li>
         </ul>
