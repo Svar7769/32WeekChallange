7e80849d74270d13fcfdd6e66097ab5a308c9e7f
Svar7769 <svarpatel7749@gmail.com>
2024-07-28 17:51:41 -0400

    url

--- finance-app/client/src/context/AppContext.js
+++ finance-app/client/src/context/AppContext.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export const AppContext = createContext();
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'finance-api-silk.vercel.app';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
