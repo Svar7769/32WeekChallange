f2d7a8ef342dc04cafdd8d3a6d971f12a9ede62e
Svar7769 <51503344+Svar7769@users.noreply.github.com>
2024-08-29 10:37:40 -0400

    Update app.js
    testing update

--- finance-app/server/app.js
+++ finance-app/server/app.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // CORS configuration
 const corsOptions = {
-  origin: 'https://financial-app-front.vercel.app', // Replace with your frontend domain
+  origin: 'financial-app-front.vercel.app', // Replace with your frontend domain
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization']
 };
