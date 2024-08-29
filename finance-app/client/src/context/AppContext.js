2ac6b3213f145e4151d637fc248f5667c5de53fc
Svar7769 <51503344+Svar7769@users.noreply.github.com>
2024-08-29 17:16:04 -0400

    Update AppContext.js
    Final Fix !!!!!

--- finance-app/client/src/context/AppContext.js
+++ finance-app/client/src/context/AppContext.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export const AppContext = createContext();
 
-const API_BASE_URL = 'https://32-week-challange.vercel.app';
+const API_BASE_URL = 'https://32-week-challange.vercel.app/api';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
